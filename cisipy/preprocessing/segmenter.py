0e0ccbf5941c6c17a256a3106171b5a90b8f5dc9
alam-shahul <alam.shahul@gmail.com>
2020-08-18 14:46:12 +0000

    Updates to segmentation code.

--- cisipy/preprocessing/segmenter.py
+++ cisipy/preprocessing/segmenter.py
@@ -1,11 +1,12 @@
 import numpy as np
 import imageio
 from scipy.ndimage import gaussian_filter
-from functools import reduce
+from scipy.sparse import csr_matrix, save_npz
+
 from pathlib import Path
 import multiprocessing as mp
 
-def merge_composites_single_sample(sample, input_directory):
+def segment_single_sample(sample, input_directory):
     """
     """
     # TODO: add docstring
@@ -26,19 +27,12 @@ def merge_composites_single_sample(sample, input_directory):
         for channel_index in composite_channel_indices:
             composite_filepaths.append((round_input_subdirectory / channels[channel_index]).with_suffix( ".tif"))
 
-    first_filepath, *other_filepaths = composite_filepaths
+    output_path = sample_input_subdirectory / "merged_composites.tif"
 
-    merged_image = imageio.imread(first_filepath)
-    for composite_filepath in other_filepaths:
-        other_image = imageio.imread(composite_filepath)
-        merged_image = np.maximum(merged_image, other_image)
+    merged_image = merge_composites(composite_filepaths, output_path)
+    return merged_image
 
-    filtered_merged_image = gaussian_filter(merged_image, 4) 
-    imageio.imwrite(sample_input_subdirectory / "merged_composites.tif", filtered_merged_image)
-
-    return filtered_merged_image
-
-def merge_composites_all_samples(config, parallelize=0):
+def segment_all_samples(config, parallelize=0):
     """
     """
     # TODO: write docstring
@@ -63,28 +57,154 @@ def merge_composites_all_samples(config, parallelize=0):
         for sample in samples:
             merge_composites_single_sample(sample, input_directory)
 
+def merge_composites(composite_filepaths, output_path):
+    """
+    """
+
+    first_filepath, *other_filepaths = composite_filepaths
+
+    merged_image = imageio.imread(first_filepath)
+    for composite_filepath in other_filepaths:
+        other_image = imageio.imread(composite_filepath)
+        merged_image = np.maximum(merged_image, other_image)
+
+    filtered_merged_image = gaussian_filter(merged_image, 4)  
+    imageio.imwrite(output_path, filtered_merged_image)
+
+    return filtered_merged_image
+    
+def parse_and_save_cell_masks(input_path, output_path):
+    """
+    """
+   
+    aggregate_masks = imageio.imread(input_path)
+    num_cells = aggregate_masks.max()
+    flattened_masks = aggregate_masks.flatten()
+    num_pixels = flattened_masks.size
+    
+    is_cell_index = (flattened_masks > flattened_masks.min())
+    binary_mask_data = np.ones(is_cell_index.sum())
+    column_indices = is_cell_index.nonzero()
+    row_indices = aggregate_masks[column_indices] - 1
+
+    compressed_cell_masks = csr_matrix((binary_mask_data, (row_indices, column_indices)), shape = (num_cells, num_pixels))
+
+    save_npz(output_path, compressed_cell_masks)
+
+    return compressed_cell_masks
+
 # def filter(im, filter_size):
-# 	if filter_size > 0:
-# 		if len(im.shape) == 3:
-# 			for i in range(im.shape[2]):
-# 				im[:,:,i] = ndimage.gaussian_filter(im[:,:,i], filter_size)
-# 		else:
-# 			im = ndimage.gaussian_filter(im, filter_size)
-# 	return im
+#   if filter_size > 0:
+#       if len(im.shape) == 3:
+#           for i in range(im.shape[2]):
+#               im[:,:,i] = ndimage.gaussian_filter(im[:,:,i], filter_size)
+#       else:
+#           im = ndimage.gaussian_filter(im, filter_size)
+#   return im
 # 
 # if __name__ == '__main__':
-# 	parser = argparse.ArgumentParser()
-# 	parser.add_argument('--basepath', help='Path to directory with subdirs for parsed images in each tissue')
-# 	parser.add_argument('--tissues', help='Comma-separated list of tissue numbers to include')
-# 	parser.add_argument('--filter-size', help='Size of gaussian filter',type=float,default=4)
-# 	parser.add_argument('--stitched-subdir', help='Subdirectory with stitched images', default='stitched_aligned_filtered')
-# 	args,_ = parser.parse_known_args()
-# 	for t in args.tissues.split(','):
-# 		tissue = 'tissue%s' % t
-# 		FP = glob.glob(os.path.join(args.basepath,tissue,args.stitched_subdir,'Composite_*'))
-# 		im = imageio.imread(FP[0])
-# 		for fp in FP[1:]:
-# 			im = np.maximum(im,imageio.imread(fp))
-# 		im = filter(im, args.filter_size)
-# 		imageio.imwrite('%s/%s/%s/All_Composite.tiff' % (args.basepath,tissue,args.stitched_subdir),im)
-# 		print(tissue)
+#   parser = argparse.ArgumentParser()
+#   parser.add_argument('--basepath', help='Path to directory with subdirs for parsed images in each tissue')
+#   parser.add_argument('--tissues', help='Comma-separated list of tissue numbers to include')
+#   parser.add_argument('--filter-size', help='Size of gaussian filter',type=float,default=4)
+#   parser.add_argument('--stitched-subdir', help='Subdirectory with stitched images', default='stitched_aligned_filtered')
+#   args,_ = parser.parse_known_args()
+#   for t in args.tissues.split(','):
+#       tissue = 'tissue%s' % t
+#       FP = glob.glob(os.path.join(args.basepath,tissue,args.stitched_subdir,'Composite_*'))
+#       im = imageio.imread(FP[0])
+#       for fp in FP[1:]:
+#           im = np.maximum(im,imageio.imread(fp))
+#       im = filter(im, args.filter_size)
+#       imageio.imwrite('%s/%s/%s/All_Composite.tiff' % (args.basepath,tissue,args.stitched_subdir),im)
+#       print(tissue)
+# if __name__ == '__main__':
+#   parser = argparse.ArgumentParser()
+#   parser.add_argument('--basepath', help='Path to directory with subdirs for parsed images in each tissue')
+#   parser.add_argument('--tissues', help='Comma-separated list of tissue numbers to include')
+#   parser.add_argument('--mask-file', help='Mask file (or prefix), eg ExpandedNuclei_* or DAPI.tiff', default='All_Composite.tiff')
+#   args,_ = parser.parse_known_args()
+#   for t in args.tissues.split(','):
+#       tissue = 'tissue%s' % t
+#       im = imageio.imread('%s/%s/segmented/%s' % (args.basepath,tissue,args.mask_file)).flatten()
+#       col_ind = np.where(im > im.min())[0]
+#       data = np.ones(len(col_ind),dtype=np.float32)
+#       row_ind = im[col_ind].astype(np.int)-1
+#       X = csr_matrix((data,(row_ind,col_ind)),shape=[len(np.unique(row_ind)),len(im)],dtype=np.float32)
+#       save_npz('%s/%s/segmented/cell_masks.npz' % (args.basepath,tissue),X)
+#       print(tissue,X.shape,len(data))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--basepath', help='Path to directory with subdirs for parsed images in each tissue')
+    parser.add_argument('--tissues', help='Comma-separated list of tissue numbers to include')
+    parser.add_argument('--stitched-subdir', help='Subdirectory with stitched images', default='stitched_aligned_filtered')
+    parser.add_argument('--mask-basepath', help='Optional alternative basepath for cell masks', default=None)
+    parser.add_argument('--as-average', dest='as_average', action='store_true')
+    parser.add_argument('--binary', dest='binary', action='store_true')
+    parser.set_defaults(as_average=False)
+    parser.set_defaults(binary=False)
+    args,_ = parser.parse_known_args()
+
+    CellMasks = load_npz('%s/%s/segmented/cell_masks.npz' % (args.mask_basepath,tissue))
+    if args.as_average:
+        for i in range(CellMasks.shape[0]):
+            ii = CellMasks.indptr[i]
+            ij = CellMasks.indptr[i+1]
+            CellMasks.data[ii:ij] = 1/(ij-ii)
+
+    FP = glob.glob(os.path.join(args.basepath,tissue,args.stitched_subdir,'*.tiff'))
+    FP_composites = [fp for fp in FP if 'Composite_' in fp.split('/')[-1]]
+    FP_direct = [fp for fp in FP if (('Composite_' not in fp.split('/')[-1]) and ('All_Composite' not in fp.split('/')[-1]) and ('DAPI' not in fp.split('/')[-1]) and ('merged' not in fp.split('/')[-1]))]
+    # sort the composites by number
+    FP_composites = [(int(fp.split('_')[-1].split('.')[0]),fp) for fp in FP_composites]
+    FP_composites = [fp[1] for fp in sorted(FP_composites)]
+    X = []
+    L = []
+    for fp in FP_composites:
+        im = imageio.imread(fp).flatten()
+        if args.binary:
+            im = (im > 0).astype(im.dtype)
+        X.append(CellMasks.dot(im))
+        L.append(fp.split('/')[-1].split('.')[0])
+    if args.as_average:
+        if args.binary:
+            savepath = '%s/%s/average_intensity_binary' % (args.basepath,tissue)
+        else:
+            savepath = '%s/%s/average_intensity' % (args.basepath,tissue)
+    else:
+        if args.binary:
+            savepath = '%s/%s/integrated_intensity_binary' % (args.basepath,tissue)
+        else:
+            savepath = '%s/%s/integrated_intensity' % (args.basepath,tissue)
+    _=os.system('mkdir %s' % savepath)
+    np.save('%s/composite_measurements.npy' % savepath,X)
+    np.save('%s/composite_labels.npy' % savepath,L)
+    X = []
+    L = []
+    # get the size of cell masks
+    FP_mask = glob.glob(os.path.join(args.mask_basepath,tissue,'segmented','ExpandedNuclei_*'))
+    if len(FP_mask) == 0:
+        FP_mask = glob.glob(os.path.join(args.mask_basepath,tissue,'segmented','All_Composite.tiff'))
+    n = imageio.imread(FP_mask[0]).shape
+    for fp in FP_direct:
+        im = imageio.imread(fp)
+        # images might have been padded on the bottom and right
+        im = im[:n[0],:n[1]].flatten()
+        if args.binary:
+            im = (im > 0).astype(im.dtype)
+        X.append(CellMasks.dot(im))
+        L.append('.'.join(fp.split('/')[-1].split('.')[:-1]))
+    if (tissue == 'tissue2') and ('Foxp2' in L): # Foxp2 was measured twice in tissue2
+        gi = L.index('Foxp2')
+        X.append(X[gi])
+        L.append(L[gi])
+    if args.as_average:
+        np.save('%s/direct_measurements.npy' % savepath,X)
+        np.save('%s/direct_labels.npy' % savepath,L)
+    else:
+        np.save('%s/direct_measurements.npy' % savepath,X)
+        np.save('%s/direct_labels.npy' % savepath,L)
+
+
+
+
