1908966d236ca78d731cea0137713584d6099e69
Shahul Alam <alam.shahul@gmail.com>
2020-08-21 10:26:05 +0000

    Getting rid of spams requirement for readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -56,3 +56,6 @@ html_theme = 'pyramid'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Autodoc config
+autodoc_mock_imports = ["spams"]
