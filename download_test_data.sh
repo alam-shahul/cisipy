fd97b26e6bf1bda9b0e638dde12e9d9c824c23e4
alam-shahul <alam.shahul@gmail.com>
2020-08-21 17:13:24 +0000

    Added download script and workflow directory.

--- download_test_data.sh
+++
@@ -1,4 +0,0 @@
-TEST_NAME=$1
-DATA_DIRECTORY=$2
-
-gsutil cp -r gs://fc-5de89d3c-1dab-43ba-81fc-7c15266efb43/data/$TEST_NAME $DATA_DIRECTORY
---
+++ workflows/download_test_data.sh
@@ -0,0 +1,5 @@
+TEST_NAME=$1
+DATA_DIRECTORY=$2
+
+mkdir $DATA_DIRECTORY
+gsutil cp gs://fc-5de89d3c-1dab-43ba-81fc-7c15266efb43/data/$TEST_NAME/* $DATA_DIRECTORY
